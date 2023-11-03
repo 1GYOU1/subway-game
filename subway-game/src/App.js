9111a884b883967f3d5ac11aec80f892e194f349
1gyou1 <1gyou1@naver.com>
2023-11-03 23:19:30 +0900

    update

---
+++ subway-game/src/App.js
@@ -0,0 +1,24 @@
+import { Routes, Route } from "react-router-dom";
+import React from 'react';
+
+import Intro from "./components/Intro";
+import Start from "./components/Start";
+import Game from "./components/Game";
+import Result from "./components/Result";
+
+import './css/style.css';
+
+function App() {
+    return (
+        <div>
+            <Routes>
+                <Route path="/" element={<Intro />} />
+                <Route path="/start" element={<Start />} />
+				<Route path="/game" element={<Game />} />
+                <Route path="/result" element={<Result />} />
+            </Routes>
+        </div>
+    );
+}
+
+export default App;
