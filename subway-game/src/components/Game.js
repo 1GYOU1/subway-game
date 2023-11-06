4d64657620f2a98794205254d4d5eefafe407eda
1gyou1 <1gyou1@naver.com>
2023-11-06 21:26:12 +0900

    2023-11-06 정답제출

--- subway-game/src/components/Game.js
+++ subway-game/src/components/Game.js
@@ -1,6 +1,8 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 // image
+import submitBtnImg from '../img/btn_submit.png';
+import clickImg from '../img/click.png';
 import correctImg from '../img/scoring1.png';
 import levelImg1 from '../img/level1.png';
 import retryBtnImg from '../img/retry_btn.png';
@@ -8,6 +10,8 @@ import retryBtnImg from '../img/retry_btn.png';
 const Game = () => {
 
     let navigate = useNavigate();
+
+    let clickRef = useRef();
     
     const [randomLine, setRandomLine] = useState(Math.floor(Math.random() * 4+1))// 랜덤 호선 1~4
     const [inputValue, setInputValue] = useState(''); // input 데이터 입력
@@ -17,24 +21,27 @@ const Game = () => {
     const [timer, setTimer] = useState(10);// 타이머
 
     /*
-        1. 0~4중에 랜덤으로 숫자 노출 - 한 문제마다 랜덤으로 추가
-        2. api 가져오기 - 맨 처음 한번만 가져오기.
-            ㄴ 형식 {[1, 평택], [1, 금정]}
-        3. 서울역 예외처리
-        4. input 입력 텍스트 받아오기
+        1. 0~4중에 랜덤으로 숫자 노출 - 한 문제마다 랜덤 돌리기
+        2. api 맨 처음 한번만 가져오기. ex - {['01호선', '평택역']}
+            ㄴ 필요한 형태로 가져오기{{ LINE_NUM, STATION_NM }, [1, 평택], [1, 금정]}
+            ㄴ 서울역 예외처리-=98
+        3. input 입력 텍스트 받아오기
+            ㄴ입력한 값 useState에 넣고(inputValue) 특수문자, 영어 입력 제한
+        4. input에 답안 입력 후 엔터키, 클릭 시 제출
 
-        5. 입력한 값 useState에 넣고 api 요소의 0번째 배열 (호선) 비교해서 맞는지 체크
-        6. input에 입력하여 정답이 맞을 경우 setCorrect에 저장해두어 중복 체크 해주기.
-        
-        7. 정답, 오답 이미지 노출
-        8. 10문제만 진행 quizCount++
 
-        9. 타이머기능 10초
-        10. useParams 로 결과 값 넘겨주는 방식 해보기 안되면 props로 전달
+        5. 제출한 입력값이 api 요소의 호선(0번째 배열 요소값), 역 이름(1번째 배열 요소값)이 비교해서 맞으면 배열에 저장
+        6. input에 입력하여 정답이 맞을 경우 setCorrect에 저장해두기 
+        7. 맞은 답안이 api에 또 있으면 중복 체크 해주기.
+        8. 정답, 오답 구분
+        9. 정답, 오답 이미지 노출
+        10. 10문제 진행 quizCount++
+        11. 타이머기능 10초
+        11. useParams 로 결과 값 넘겨주는 방식 해보기 안되면 props로 전달
 
         --------------------
-        11. 힌트 기능
-            ㄴ 지하철 노선도 이미지 노출
+        12. 힌트 기능
+            ㄴ 지하철 노선도 이미지 노출 ?
     */
 
     //Api
@@ -59,7 +66,7 @@ const Game = () => {
                 });
                 // LINE_NUM과 STATION_NM만 추출하여 배열에 저장
                 const extractedData = stationList.map(({ LINE_NUM, STATION_NM }) => {
-                    // 예외처리
+                    LINE_NUM = parseInt(LINE_NUM.replace("호선", "").trim());
                     if (STATION_NM === "서울역") {
                       STATION_NM = "서울";
                     }
@@ -80,36 +87,75 @@ const Game = () => {
         console.log(stationData)
     }, [stationData])
 
-
-    // 정답 입력
-    const handleSubmit = (event) => {
-        event.preventDefault(); // 기본동작 새로고침 막기
-    };
-
-    const handleKeyUp = (event) => {
-        if (event.key === 'Enter') {
-            event.preventDefault();
-            console.log('내가 입력한 답안:', inputValue);
-        }
-    };
-
-    const handleInputChange = (event) => {
-        // input 값이 변경될 때마다 inputValue 상태를 업데이트
-        setInputValue(event.target.value);
-    };
-    
-
-    // 결과 페이지로 이동
-    const goResult = () => {
-        navigate('/result');
-    }
-
     if (loading) {
         return <div className='loading'>Loading...</div>;
     }
     
     if (error) {
     return <div className='loading'>Error: {error.message}</div>;
+    }
+
+    // 정답 입력
+    const handleSubmit = (event) => {
+        event.preventDefault(); // 기본동작 새로고침 막기
+    };
+
+    const handleInputChange = (event) => {
+        // 영어, 특수문자, 스페이스바(공백) 입력 방지
+        const englishRegExp = /[A-Za-z]/;
+        const specialCharRegExp = /[\{\}\[\]\/?.,;:|\)*~`!^\-_+┼<>@\#$%&'"\(\)=]/;
+        const spaceRegExp = /\s/;
+    
+        if (englishRegExp.test(event.target.value)) {
+            alert('영어는 입력할 수 없습니다.');
+            return;
+        }
+        if (specialCharRegExp.test(event.target.value)) {
+            alert('특수문자는 입력할 수 없습니다.');
+            return;
+        }
+        if (spaceRegExp.test(event.target.value)) {
+            alert('스페이스바(공백)는 입력할 수 없습니다.');
+            return;
+        }
+
+        // 입력값에서 영어, 특수문자, 스페이스바(공백)를 제외한 값을 추출
+        const lastSubmitValue = event.target.value
+            .replace(englishRegExp, '')
+            .replace(specialCharRegExp, '')
+            .replace(spaceRegExp, '')
+            .trim();
+
+        //제출하기 클릭 애니메이션 노출
+        if(lastSubmitValue.length > 0){
+            clickRef.current.classList.add('on');
+        }else{
+            clickRef.current.classList.remove('on');
+        }
+        // input 값이 변경될 때마다 inputValue 상태를 업데이트
+        setInputValue(lastSubmitValue);
+    };
+    
+    // 엔터키 정답 제출
+    const handleKeyUp = (event) => {
+        if (event.key === 'Enter') {
+            event.preventDefault();
+            console.log('내가 입력한 답안:', inputValue);
+            clickRef.current.classList.remove('on');
+            setInputValue(''); // input 초기화
+        }
+    };
+
+    //버튼 클릭 정답 제출
+    const buttonClick = () => {
+        console.log('내가 입력한 답안:', inputValue);
+        clickRef.current.classList.remove('on');
+        setInputValue(''); // input 초기화
+    };
+    
+    // 결과 페이지로 이동
+    const goResult = () => {
+        navigate('/result');
     }
 
     return (
@@ -132,8 +178,11 @@ const Game = () => {
                                 onChange={handleInputChange}
                             />
                         </label>
-                        <button id="answer_btn" type="submit">제출</button>
                     </form>
+                    <button id="answer_btn" type="submit" onClick={buttonClick}>
+                        <img ref={clickRef} className='click' src={clickImg} alt="클릭"/>
+                        <img src={submitBtnImg} alt="정답 제출하기"/>
+                    </button>
                     {/* 타이머 이미지 */}
                     {/* <img id="timer" className="timer" src={`./img/timer_${timer}s.png`} alt="타이머"/> */}
                 </div>
